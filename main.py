fea7939cbedf0aa0e332f3e99a97eecd7bfbb7bd
nagendranice <60419186+nagendranice@users.noreply.github.com>
2021-12-02 22:48:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,10 +51,12 @@ with header:
    
 
 with body:
+    rawtext = st.text_area('Enter Text Here')
+  
   
     sample_col, upload_col = st.beta_columns(2)
     
-    
+    upload_col.subheader(' [OR] ')
     uploaded_file = upload_col.file_uploader(
         'Choose your .txt file', type="txt")
     if uploaded_file is not None:
