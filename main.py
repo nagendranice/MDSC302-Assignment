158c3aaceecd7748a4028ea2e3579cb4eda83e5d
nagendranice <60419186+nagendranice@users.noreply.github.com>
2021-12-02 22:33:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ with header:
     titl, imga = st.beta_columns(2)
     titl.title('UNO SDG CLASSIFIER')
     imga.image('1sdg_logo.svg.gif')
-    upload_col = st.beta_columns(1)
+    upload_col = st.beta_columns(2)
    
     uploaded_file = upload_col.file_uploader(
         'Choose your .txt file', type="txt")
