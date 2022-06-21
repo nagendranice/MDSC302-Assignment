b3e07286c723a6915d09896316ac25a23649d6fd
nagendranice <60419186+nagendranice@users.noreply.github.com>
2022-06-21 11:17:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,29 +46,7 @@ def classify(a):
 with header:
     titl, imga = st.beta_columns(2)
     titl.title('UNO - SDG Classifier')
-    import base64
-
-    @st.cache(allow_output_mutation=True)
-    def get_base64_of_bin_file(bin_file):
-      with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-    def set_png_as_page_bg(png_file):
-     bin_str = get_base64_of_bin_file(png_file)
-     page_bg_img = '''
-     <style>
-     body {
-     background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-     background-size: cover;
-     }
-     </style>
-    ''' % bin_str
     
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-    set_png_as_page_bg('background.png')
    
 with body:
     page_bg_img = '''
